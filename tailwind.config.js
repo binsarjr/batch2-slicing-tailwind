f23c1e48fbc45bdc55f2954e148703c17c79d4fe
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-23 19:41:29 +0700

    done: slicing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#00ADB5',
+        secondary: '#393E46BF'
+      }
+    },
+
   },
   plugins: [],
 }
-
